1d78ae3cda09934c159855b4e82cdd2a8c55b16d
Gaith <alasmargaith@gmail.com>
2025-08-05 16:53:23 +0300

    shortcut

--- shop/main.js
+++ shop/main.js
@@ -54,39 +54,7 @@ function addEvents(){
 }
 
 // =========== HANDLE EVENTS =========== //
-function handle_addCartItem() {
-    let product = this.parentElement;
-    let title = product.querySelector('.product-title').innerHTML;
-    let price = product.querySelector('.product-price').innerHTML;
-    let imgSrc = product.querySelector('.product-img').src;
-    
-    let newToAdd = {
-        title,
-        price,
-        imgSrc,
-    };
 
-    // Add product to cart
-    let cartBoxElement = CartBoxComponent(title, price, imgSrc);
-    let newNode = document.createElement('div');
-    newNode.innerHTML = cartBoxElement;
-    const cartContent = cart.querySelector('.cart-content');
-    cartContent.appendChild(newNode);
-
-    update();
-}
-
-function handle_removeCartItem() {
-    this.parentElement.remove();
-    update();
-}
-
-function handle_changeItemQuantity() {
-    if (isNaN(this.value) || this.value < 1) {
-        this.value = 1;
-    }
-    update();
-}
 
 function handle_buyOrder(){
     if(itemsAdded.length <= 0) {
@@ -100,40 +68,6 @@ function handle_buyOrder(){
 
     update();
 }
-
-
-// =========== UPDATE & RERENDER FUNCTIONS =========== //
-function updateTotal() {
-    let cartBoxes = document.querySelectorAll('.cart-box');
-    const totalElement = cart.querySelector('.total-price');
-    let total = 0;
-
-    cartBoxes.forEach((cartBox) => {
-        let priceElement = cartBox.querySelector('.cart-price');
-        let price = parseFloat(priceElement.innerHTML.replace('$', ''));
-        let quantity = cartBox.querySelector('.cart-quantity').value;
-        total += price * quantity;
-    });
-
-    total = total.toFixed(2);
-    totalElement.innerHTML = '$' + total;
-}
-
-// =========== HTML COMPONENTS =========== //
-function CartBoxComponent(title, price, imgSrc) {
-    return `
-    <div class="cart-box">
-        <img src="${imgSrc}" alt="" class="cart-img">
-        <div class="detail-box">
-            <div class="cart-product-title">${title}</div>
-            <div class="cart-price">${price}</div>
-            <input type="number" value="1" class="cart-quantity">
-        </div>
-        <i class='bx bxs-trash-alt cart-remove'></i>
-    </div>`
-}
-
-
 
 //==============  HANDLE EVENT FUNCTION ==============//
 let itemsAdded = [];
